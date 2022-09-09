5bc7b1a30cdd4aadf82beeafb39737c42e91ba42
Thiago Medeiros <medeiroshs@hotmail.com>
2022-09-09 11:07:11 -0300

    feat: desafio 16 concluido

---
+++ challenges/desafio16.js
@@ -0,0 +1,3 @@
+db.produtos.updateMany({ nome: "Big Mac" }, 
+{ $currentDate: { ultimaModificacao: { $type: "timestamp" } } });
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { nome: 1, _id: 0 });
